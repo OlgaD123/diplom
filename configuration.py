a4cad858a49f10edf4eb72d55c14e99bbb5cba45
Olga <gug-89@mail.ru>
2024-08-20 16:15:44 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,2 +1,8 @@
-URL_SERVICE =  "https://325ec26c-4153-4689-b0de-ae5bc767194a.serverhub.praktikum-services.ru/"
-CREAT_ORDERS = "/api/v1/orders"
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+URL_SERVICE = "https://325ec26c-4153-4689-b0de-ae5bc767194a.serverhub.praktikum-services.ru/"
+
+# CREATE_ORDER хранит путь к API-методу для создания нового заказа.
+CREATE_ORDER = "/api/v1/orders"
+
+# PUT_ORDER хранит путь к API-методу для создания нового набора.
+PUT_ORDER = "/api/v1/orders/track"
